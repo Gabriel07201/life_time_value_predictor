5cb959f33a3fbbcf918a7389c7e7b17848f8c638
Gabriel Antonio de Lima <105071638+Gabriel07201@users.noreply.github.com>
2023-03-06 10:16:59 -0300

    projeto_finalizado

--- criando_json.py
+++
@@ -1,4 +0,0 @@
-import pandas as pd
-json = pd.read_csv('test_koRSKBP.csv')
-with open(r'C:\Users\Gabriel\Desktop\Programação\life_time_value_predictor\teste.json', 'w') as arquivo:
-    json.to_json(arquivo)
--- main.py
+++ main.py
@@ -8,17 +8,38 @@ import json
 
 app = FastAPI()
 
+@app.get("/")
+def home():
+    file_name = "teste.json"
+    file_path = os.getcwd() + "/" + file_name
+    return FileResponse(path=file_path, media_type='application/json', filename=file_name)
+
+
 @app.post("/predict/")
 async def create_upload_file(file: UploadFile = File(...)):
     contents = await file.read()
-    aleatoria = json.loads(contents.decode('utf-8'))
-    dados = json.dumps(aleatoria)
+    string = json.loads(contents.decode('utf-8'))
+    dados = json.dumps(string)
     pipe = Pipeline(dados)
     df_final = pipe.execute_pipe()
-    print(df_final)
+    # print(df_final)
 
     return Response(df_final)
 
 
-if __name__ == "__main__":
-    uvicorn.run("main:app", port=5000, log_level="info")
+@app.post('/df_final/')
+async def df_final(file: UploadFile = File(...)):
+    contents = await file.read()
+    string = json.loads(contents.decode('utf-8'))
+    dados = json.dumps(string)
+    pipe = Pipeline(dados)
+    df_final = pipe.execute_pipe()
+    with open('df_final.json', 'w') as f:
+        json.dump(df_final, f)
+    file_name = "df_final.json"
+    file_path = os.getcwd() + "/" + file_name
+
+    return FileResponse(path=file_path, media_type='application/json', filename=file_name)
+
+if __name__ == '__main__':
+    uvicorn.run('main:app', port=5000, log_level='info')
