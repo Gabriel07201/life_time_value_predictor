80b74250e01968f213b2d4a8c4a710b704abaca5
Gabriel Antonio de Lima <105071638+Gabriel07201@users.noreply.github.com>
2023-03-06 09:47:35 -0300

    fix_data_encoder

--- pipeline.py
+++ pipeline.py
@@ -7,9 +7,11 @@ class Pipeline:
         import joblib
         self.df = pd.read_json(data)
         self.model = joblib.load('cltv_predictor.joblib')
+        self.data_to_encode = pd.read_csv('train_BRCpofr.csv')
     
     def drop_columns(self):
         self.df = self.df.drop(['vintage', 'claim_amount', 'id'], axis=1)
+        self.data_to_encode = self.data_to_encode.drop(['vintage', 'claim_amount', 'id', 'cltv'], axis=1)
         
         return self.df
     
@@ -19,6 +21,7 @@ class Pipeline:
                     'Female' : 1,
         }
         self.df['gender'] = self.df['gender'].map(replace_gender)
+        self.data_to_encode['gender'] = self.data_to_encode['gender'].map(replace_gender)
         
         replace_type_of_policy = {
                     'Silver' : 0,
@@ -26,13 +29,15 @@ class Pipeline:
                     'Platinum' : 2
         }
         self.df['type_of_policy'] = self.df['type_of_policy'].map(replace_type_of_policy)
+        self.data_to_encode['type_of_policy'] = self.data_to_encode['type_of_policy'].map(replace_type_of_policy)
+
         
         return self.df
     
     def one_hot_encoder(self):
         from category_encoders.one_hot import OneHotEncoder
-        self.encoder = OneHotEncoder(cols=['area', 'qualification', 'income', 'num_policies', 'policy'])
-        self.encoder.fit(self.df)
+        self.encoder = OneHotEncoder(cols=['area', 'qualification', 'income', 'num_policies', 'policy'], handle_missing="value", handle_unknown="value")
+        self.encoder.fit(self.data_to_encode)
         self.df = self.encoder.transform(self.df)
 
         return self.df
@@ -58,6 +63,7 @@ class Pipeline:
 
 
 # json = pd.read_csv('test_koRSKBP.csv').to_json()
+# json = 'dados_para_prever.json'
 # pipe = Pipeline(json)
 # df_test = pipe.execute_pipe()
 # print((df_test))
--- recebendo_arquivo.py
+++ recebendo_arquivo.py
@@ -6,7 +6,8 @@ import pandas as pd
 import csv
 
 with open(r'C:\Users\Gabriel\Desktop\Programação\life_time_value_predictor\teste.json', 'rb') as arquivo:
-    response = requests.post("http://127.0.0.1:5000/predict/", files={"file": arquivo})
+    response = requests.post("https://api-cltv-portfolio.up.railway.app/predict/", files={"file": arquivo})
+
 
 json_data = json.loads(response.content.decode('utf-8'))
 
